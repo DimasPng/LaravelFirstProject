db387316147d1c69b8f3be7eb2c175a1e25d8276
dimaspng <d.sagachko@gmail.com>
2023-10-23 11:01:16 +0300

    add delete migration

--- database/migrations/2023_10_23_080050_delete_column_description_to_posts_table.php
+++ database/migrations/2023_10_23_080050_delete_column_description_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->text('description')->nullable()->after('text');
+            //
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('description');
+            //
         });
     }
 };

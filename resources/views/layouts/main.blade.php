0762ac129e7301e13e8cc9ab79b2b77859db0021
dimaspng <d.sagachko@gmail.com>
2023-10-23 15:19:38 +0300

    add bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,17 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-<div>
-    <ul>
-        <li><a href={{route('about.index')}}>About</a></li>
-        <li><a href="{{route('contact.index')}}">Contact</a></li>
-        <li><a href="{{route('item.index')}}">Item</a></li>
-        <li><a href="{{route('policy.index')}}">Policy</a></li>
-        <li><a href="{{route('post.index')}}">Posts</a></li>
-        <li><a href="{{route('main.index')}}">Main</a></li>
-    </ul>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('about.index')}}">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('contact.index')}}">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('item.index')}}">Item</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('policy.index')}}">Policy</a>
+                </li><li class="nav-item">
+                    <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                </li><li class="nav-item">
+                    <a class="nav-link" href="{{route('main.index')}}">Main</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </div>
 @yield('content')
 </body>
